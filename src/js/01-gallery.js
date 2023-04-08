61cd840cd962fbff0efb16dca0af32461c294dd1
AlexandrTarabanko <clarityabovezero@gmail.com>
2023-04-08 18:53:20 +0300

    Finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,47 +9,17 @@ const listRef = document.querySelector('.gallery'); // Привязываю сп
 const createGallery = galleryItems // Создаю разметку
   .map(
     item => `<li class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="Image description"
-    />
-  </a>
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
 </li>`
   )
   .join('');
 
 listRef.innerHTML += createGallery; //Добавляю разметку
 
-listRef.addEventListener('click', onImageClick); // ИВЕНТ ПО КЛИКУ
-
-function onImageClick(event) {
-  event.preventDefault();
-  const isImage = event.target.classList.contains('gallery__image');
-  if (!isImage) {
-    return;
-  }
-
-  const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}">`,
-    {
-      onShow: instance => {
-        document.addEventListener('keydown', onCloseByEsc);
-      },
-      onClose: instance => {
-        document.removeEventListener('keydown', onCloseByEsc);
-      },
-    }
-  );
-  instance.show();
-
-  function onCloseByEsc(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
-
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: `alt`,
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const btnRef = document.querySelector('button[type="submit"]');
+const emailInputRef = document.querySelector('input[name="email"]');
+const messageTextRef = document.querySelector('textarea[name="message"]');
+const dataObj = {};
+const STORAGE_KEY = 'feedback-form-state';
 
+populate();
+
+// Input
+
+function onInputFill(e) {
+  if (e.target.name === 'email') {
+    dataObj[e.target.name] = e.target.value;
+  } else if (e.target.name === 'message') {
+    dataObj[e.target.name] = e.target.value;
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
+}
+
+// Populate
+function populate() {
+  let localData = localStorage.getItem(STORAGE_KEY);
+  if (localData) {
+    localData = JSON.parse(localData);
+    emailInputRef.value = localData.email;
+    messageTextRef.value = localData.message;
+  }
+}
+
+// Submit
+const onClickSubmit = e => {
+  e.preventDefault();
+
+  if (emailInputRef.value === '' || messageTextRef.value === '') {
+    alert('Fill all the gaps!');
+  } else {
+    console.log(dataObj);
+    formRef.reset();
+    window.localStorage.clear();
+  }
+};
+
+formRef.addEventListener('input', throttle(onInputFill, 500));
+btnRef.addEventListener('click', onClickSubmit);
