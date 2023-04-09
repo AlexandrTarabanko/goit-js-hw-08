cb5f6a34576c16470404e50d84008d52cbfa5ed3
AlexandrTarabanko <clarityabovezero@gmail.com>
2023-04-10 00:58:40 +0300

    Hallelujah

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,10 +3,18 @@ const formRef = document.querySelector('.feedback-form');
 const btnRef = document.querySelector('button[type="submit"]');
 const emailInputRef = document.querySelector('input[name="email"]');
 const messageTextRef = document.querySelector('textarea[name="message"]');
-let dataObj = {};
 const STORAGE_KEY = 'feedback-form-state';
 
-populate();
+const actualObj = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? '';
+emailInputRef.value = actualObj.email || '';
+messageTextRef.value = actualObj.message || '';
+
+let dataObj = {
+  email: actualObj.email || '',
+  message: actualObj.message || '',
+};
+
+// populate();
 
 // Input
 
@@ -18,35 +26,6 @@ function onInputFill(e) {
   }
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
 }
-
-// Populate
-function populate() {
-  let localData = localStorage.getItem(STORAGE_KEY);
-
-  // if (localData === undefined) {
-  //   localData = JSON.parse(localData);
-  //   localData.email = '';
-  //   emailInputRef = '';
-  //   localData.message = '';
-  //   messageTextRef = '';
-  //   console.log(localData);
-  // }
-
-  if (localData) {
-    localData = JSON.parse(localData) || '';
-    // console.log(localData); // log object in localStorage
-    // if (!localData.email === undefined) {
-    //   localData.email = '';
-    //   console.log('ТУТ НЕ БЫЛО ПОЧТЫ');
-    // } else if (!localData.message) {
-    //   localData.message = '';
-    //   console.log('ТУТ НЕ БЫЛО ТЕКСТА');
-    // }
-    emailInputRef.value = localData.email || '';
-    messageTextRef.value = localData.message || '';
-  }
-}
-
 // Submit
 const onClickSubmit = e => {
   e.preventDefault();
@@ -58,9 +37,8 @@ const onClickSubmit = e => {
   ) {
     alert('Fill all the gaps!');
   } else {
-    dataObj.email = emailInputRef.value;
-    dataObj.message = messageTextRef.value;
-    console.log(dataObj);
+    const localData = localStorage.getItem(STORAGE_KEY);
+    console.log(localData);
     formRef.reset();
     window.localStorage.clear();
     dataObj = {};
